8616d6f807929c19541d369b8d132f3520942465
vladimir diakonov <8840438@gmail.com>
2020-11-23 16:46:05 +0300

    done task1.3 fix task1.1 task1.2

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -11,20 +11,23 @@ function averageMark(a, g, p) {
 	let algebra = a;
 	let geography = g;
 	let physics = p;
-	taskResult = ((a + g + p) / 3)
-	return taskResult// ваш ответ;
+	taskResult = ((a + g + p) / 3);
+	return taskResult;// ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
   let myName = userName;  
-	return (`Привет, мир! Меня зовут ${myName}`) // ваш ответ;
+	return (`Привет, мир! Меня зовут ${myName}`); // ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;  
+    return result; // ваш ответ;
 }
