9ef0522edf29f887fdc1b6834397f0017fcbf9f9
vladimir diakonov <8840438@gmail.com>
2020-11-30 17:30:10 +0300

    done task 1.2.1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,5 +1,16 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
+    'use strict'
+    let discr = (b ** 2 - 4 * a * c);
+    let x;
+    let x1;
+    if (discr < 0) {
+        return [];
+    } else if (discr === 0) {
+        return [x = ((-b + Math.sqrt(discr)) / 2 * a)];
+    } else {
+        return [x = ((-b + Math.sqrt(discr)) / 2 * a), x1 = ((-b - Math.sqrt(discr)) / 2 * a)];
+    }
     // return x;
 }
 
