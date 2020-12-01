c1138471401310d9680deecb57efadd07e3d82b8
vladimir diakonov <8840438@gmail.com>
2020-12-01 14:16:22 +0300

    fixed all homeworks 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,40 +1,41 @@
+'use strict'
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    'use strict'
     let discr = (b ** 2 - 4 * a * c);
-    let x;
-    let x1;
     if (discr < 0) {
         return [];
     } else if (discr === 0) {
-        return [x = ((-b + Math.sqrt(discr)) / 2 * a)];
+        return [((-b + Math.sqrt(discr)) / 2 * a)];
     } else {
-        return [x = ((-b + Math.sqrt(discr)) / 2 * a), x1 = ((-b - Math.sqrt(discr)) / 2 * a)];
+        return [((-b + Math.sqrt(discr)) / 2 * a), ((-b - Math.sqrt(discr)) / 2 * a)];
     }
     // return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    if (marks.length === 0) {
+    let marksSum = 0;
+    if (marks.length === 0){
         return 0;
-    } else if (marks.length === 1) {
-        return marks[0];
-    } else if (marks.length === 2) {
-        return (marks[0] + marks[1]) / marks.length;
-    } else if (marks.length === 3) {
-        return (marks[0] + marks[1] + marks[2]) / marks.length;
-    } else if (marks.length === 4) {
-        return (marks[0] + marks[1] + marks[2] + marks[3]) / marks.length;
-    } else return (marks[0] + marks[1] + marks[2] + marks[3] + marks[4]) / 5;
+    } else if (marks.length > 5) {
+        let firstFiveMarks = marks.slice(0, 5);
+        for (let i = 0; i < firstFiveMarks.length; i++) {
+            marksSum += firstFiveMarks[i];
+        }
+        return marksSum / firstFiveMarks.length;
+    } else for (let i = 0; i < marks.length; i++) {
+        marksSum += marks[i];
+    }
+    return marksSum / marks.length;
     // return averageMark;
 }
 
+
 function askDrink(name, dateOfBirthday){
     // код для задачи №3 писать здесь
-    let birthday = new Date(dateOfBirthday);
-    let yearNow = new Date();
-    let age = (yearNow.getFullYear() - birthday.getFullYear());
-    return age;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+    if (age > 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     // return result;
 }
