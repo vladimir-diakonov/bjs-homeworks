84d3851f13126b333c16c8bac7f1475ffb7cbbf8
vladimir diakonov <8840438@gmail.com>
2020-12-07 14:10:06 +0300

    task 2.1.1 finished

---
+++ 2.1-functions/task.js
@@ -0,0 +1,45 @@
+//task 2.1.1
+function getSolutions(a, b, c) {
+  let D = b ** 2 - 4 * a * c;
+  let x0 = [];
+  let x1 = (-b + Math.sqrt(D)) / 2 * a;
+  let x2 = (-b - Math.sqrt(D)) / 2 * a;
+  //let roots = {
+    //x0,
+    //x1,
+    //x2
+  //};
+
+  if (D < 0) {
+    return {
+      D,
+      x0
+    }
+  } else if (D > 0) {
+    return {
+      D,
+      x1,
+      x2
+    }
+  } else return {
+    D,
+    x1
+  };
+};
+
+function showSolutionsMessage( a, b, c ) {
+  let result = getSolutions(a, b, c)
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(result.D);
+  consolr.log(`Значение дискриминанта ${result.D}`);
+  if (result.D < 0) {
+    console.log(`Уравнение не имеет вещественных корней`);
+  } else if (result.D > 0) {
+    console.log(`Уравнение имеет два корня. X₁ = ${result.x1}, X₂ = ${result.x2}`);
+  } else console.log(`Уравнение имеет один корень X₁ = ${result.x1}`);
+};
+
+//task 2.1.2
+function getAverageScore(data) {
+
+};
