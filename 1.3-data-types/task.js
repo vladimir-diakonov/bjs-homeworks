9c0267d2f20d65fab38cf6f320f1f25af58effaa
vladimir diakonov <8840438@gmail.com>
2020-12-08 14:54:19 +0300

    tasks fix

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,9 +2,10 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
     let S = amount - contribution;
-    let n = (date.getMonth() - new Date().getMonth()) + ((date.getFullYear() - new Date().getFullYear()) * 12);
-    let P = percent / 12;
-    const payment = S * (P+P / (((1 + P) ** n) - 1));
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date ().getMonth());
+    let p = percent / 1200;
+    let monthPayment = S * (p + p / ((Math.pow((1 + p), n)) - 1));
+    let payment = monthPayment * n;
     if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
         return 'все поля должны содержать числа';
     } else return +payment.toFixed(2);
@@ -16,4 +17,4 @@ function getGreeting(name) {
     let nameCheck = name.trim();
     return `Привет, мир! Меня зовут ${name || 'Аноним'}.`
     // return greeting;
-}
+};
