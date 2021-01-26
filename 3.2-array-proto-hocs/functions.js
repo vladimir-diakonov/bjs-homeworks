eb087409e0d97dad2c084a21121ea1f6aa304879
vladimir diakonov <8840438@gmail.com>
2021-01-26 14:34:03 +0300

    tasks 3.2.2 done

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -23,3 +23,38 @@ function getValuestCountToSumValues(arr, value) {
 }
 
 //task 3.2
+
+function sleep(milliseconds) 
+{
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+}
+
+function sum(...args) {
+  // Замедление на половину секунды.
+  sleep(100); // Можно использовать другое значение замедления.
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
+}
+
+let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+
+function memorize(fn, limit) {
+  let memory = [];
+  return (...args) => {
+    let result = memory.find(answer => compareArrays(answer.args, args));
+    if (result) {
+      return result.result;
+    } else {
+      result = fn(...args);
+      memory.push({
+        args: args,
+        result: result
+      });
+      if (memory.length > limit) {
+        memory.shift();
+      } return result;
+    }
+  }
+}
